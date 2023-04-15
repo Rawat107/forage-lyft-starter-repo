2caa6f9c463b6b84ae13647ac80652e69369c987
Vaibhav Rawat <97122691+Rawat107@users.noreply.github.com>
2023-04-15 11:43:35 +0530

    Task Three

--- CarFactory.py
+++ CarFactory.py
@@ -1,42 +1,41 @@
 from abc import ABC
 
-from engine import capulet_engine
-from engine import sternman_engine
-from engine import willoughby_engine
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
 
-from battery import spindlerBattery
-from battery import nubbinBattery
+from battery.spindlerBattery import SpindlerBattery
+from battery.nubbinBattery import NubbinBattery
 
 from car import Car
 
 class CarFactory(ABC):
   def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
     return Car (
-      capulet_engine(last_service_mileage, current_mileage),
-      spindlerBattery(last_service_date)
+      CapuletEngine(last_service_mileage, current_mileage),
+      SpindlerBattery(last_service_date)
     )
 
   def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
     return Car (
-      willoughby_engine(last_service_mileage, current_mileage),
-      spindlerBattery(last_service_date)
+      WilloughbyEngine(last_service_mileage, current_mileage),
+      SpindlerBattery(last_service_date)
     )
 
   def create_palindrome(current_date, last_service_date, warning_light_is_on):
     return Car (
-      sternman_engine(warning_light_is_on),
-      spindlerBattery(last_service_date)
+      SternmanEngine(warning_light_is_on),
+      SpindlerBattery(last_service_date)
     )
 
   def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
     return Car (
-      willoughby_engine(last_service_mileage, current_mileage),
-      nubbinBattery(last_service_date)
+      WilloughbyEngine(last_service_mileage, current_mileage),
+      NubbinBattery(last_service_date)
     )
 
   def create_thorvex(current_date, last_service_date, current_mileage, last_service_mileage):
     return Car(
-      capulet_engine(last_service_mileage, current_mileage),
-      nubbinBattery(last_service_date)
+      CapuletEngine(last_service_mileage, current_mileage),
+      NubbinBattery(last_service_date)
     )
-  
